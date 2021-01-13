f798731bc8202de368c63929901ab8b3d68fffc9
CartoonM <newfuturama@outlook.com>
2021-01-13 14:53:09 +0200

    add scroll and animation

--- src/components/NoteItem/NoteItem.js
+++ src/components/NoteItem/NoteItem.js
@@ -1,10 +1,30 @@
+import {useEffect, useState} from 'react';
+import ScrollIntoViewIfNeeded from 'react-scroll-into-view-if-needed';
 import classes from './NoteItem.module.scss';
 
 const NoteItem = props => {
+
+    const defaultCls = [classes.NoteItem];
+
+    if (props.setDelay) {
+        defaultCls.push(classes.hide);
+    }
+
+    const [cls, setCls] = useState(defaultCls);
+
+    useEffect(() => {
+        if (cls.length > 1) {
+            setTimeout(() => setCls([classes.NoteItem]), 250);
+        }
+    }, [])
+
     return (
-        <div className={classes.NoteItem}>
+        <ScrollIntoViewIfNeeded
+          elementType="div"
+          className={cls.join(' ')}
+        >
             <textarea disabled="disabled" value={props.text} />
-        </div>
+        </ScrollIntoViewIfNeeded>
     )
 }
 
