ab038a67574e51734d25ece763b0ec6094a33e26
CartoonM <newfuturama@outlook.com>
2021-01-13 14:51:57 +0200

    add button centering

--- src/components/NoteInput/NoteInput.js
+++ src/components/NoteInput/NoteInput.js
@@ -2,12 +2,18 @@ import classes from './NoteInput.module.scss';
 
 const NoteInput = props => {
 
+    let btnCls = null;
+
+    if (props.isBlink) {
+        btnCls = classes.error;
+    }
+
     return (
         <div className={classes.NoteInput}>
             <form>
                 <textarea onChange={props.onChange} value={props.value}/>
-                <button onClick={props.onClick}>
-                    <span>U</span>
+                <button onClick={props.onClick} className={btnCls}>
+                    <div>U</div>
                 </button>
             </form>
         </div>
