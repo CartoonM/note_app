2a5245cfdfc0bc88f0c3c412614fc7f85076fe77
CartoonM <newfuturama@outlook.com>
2021-01-14 23:03:36 +0200

    add hide login form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import {connect} from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import NoteList from './containers/NoteList/NoteList';
+import Login from './containers/Login/Login';
 
-function App() {
+function App({ token }) {
   return (
     <div className="App">
       <Layout>
+        {!token ? <Login /> : null}
         <NoteList />
       </Layout>
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    token: state.auth.get('token')
+  }
+}
+
+export default connect(mapStateToProps, null)(App);
