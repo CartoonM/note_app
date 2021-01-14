9b6e64e44d3faf146a37b165bc1cc6bfad4b350b
CartoonM <newfuturama@outlook.com>
2021-01-14 23:56:50 +0200

    change addNote function, add fetchNotes and saveNotes

--- src/store/actions/notes.js
+++ src/store/actions/notes.js
@@ -1,8 +1,39 @@
-import {ADD_NOTE} from './actionTypes';
+import axiosNoteApi from '../../axios/axiosNoteApi';
+import {SAVE_NOTEs} from './actionTypes';
 
 export function addNote(note) {
+    return async (dispatch, getState) => {
+        try {
+            await axiosNoteApi.post(
+                '/notes/create/',
+                {note: note},
+                {headers: {'auth-token': getState().auth.get('token')}}
+            );
+            dispatch(fetchNotes());
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+export function fetchNotes() {
+
+    return async (dispatch, getState) => {
+        try {
+            const response = await axiosNoteApi.get(
+                '/notes/list/',
+                {headers: {'auth-token': getState().auth.get('token')}}
+                )
+            dispatch(saveNotes(response.data));
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+export function saveNotes(notes) {
     return {
-        type: ADD_NOTE,
-        note
+        type: SAVE_NOTEs,
+        notes
     }
 }
